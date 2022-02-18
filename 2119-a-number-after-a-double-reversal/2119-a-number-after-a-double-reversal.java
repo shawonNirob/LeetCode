dd167fc08cf1c317c58a616dfb3a4a58e801ad05
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-02-18 14:34:35 +0600

    Time: 1 ms (21.97%), Space: 40.9 MB (6.02%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-            int count = 2, r;
-            int number = num;
-            while (count > 0) {
-
-                int sum=0;
-                while (num > 0) {
-                    r = num % 10;
-                    sum = sum*10+r;
-                    num=num/10;
-                }
-                num=sum;
-                count--;
-            }
-            if (number == num) {
-                return true;
-            }
+           if(num==0){
+               return true;
+           }
+        else if(num%10==0){
             return false;
         }
+        else{
+            return true;
+        }
+    }
 }
