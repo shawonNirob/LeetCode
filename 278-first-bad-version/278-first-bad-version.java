07c9523477b489f1f6f12fcdd33243598fd64162
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-02-20 02:39:58 +0600

    Time: 21 ms (42.68%), Space: 40.5 MB (24.14%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -4,16 +4,14 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int h=n,l=1, mid;
-        int result=0;
-        while(l<=h){
+        while(l<h){
             mid=l+(h-l)/2;
             if(isBadVersion(mid)){
-                result=mid;
-                h=mid-1;
+                h=mid;
             }else{
                 l=mid+1;
             }
         }
-        return result;
+        return l;
     }
 }
