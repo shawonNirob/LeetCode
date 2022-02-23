5479946ae5c3c0091d837ddf3cb636a3e663986e
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-02-24 04:15:13 +0600

    Time: 7 ms (97.71%), Space: 237.4 MB (85.66%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -11,22 +11,25 @@
 class Solution {
     public ListNode mergeNodes(ListNode head) {
         ListNode curr=head;
+        ListNode add =head;
         ListNode prev=head;
-
+        int sum=0;
         while(curr.next!=null){
             prev=curr;
-            int sum=0; 
             curr=curr.next;
+            if(curr.val==0){
+                add.val=sum;
+                if(prev.next.next!=null){
+                    add.next=prev.next;
+                    add=curr;
+                }else{
+                    add.next=null;
+                }
+                sum=0;
             
-            while(curr.val!=0){
-                sum +=curr.val;
-                curr=curr.next; 
+            }else{
+                sum+=curr.val;
             }
-            prev.val=sum;
-            prev.next=curr;
-        }
-        if(curr.next==null){
-            prev.next=null;
         }
         return head;
     }
