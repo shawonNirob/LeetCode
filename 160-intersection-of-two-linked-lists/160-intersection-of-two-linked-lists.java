dce4c331a45117ef46b84bf7cc439e27921c3392
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-02-24 13:55:29 +0600

    Time: 228 ms (6.90%), Space: 55.4 MB (18.55%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -15,16 +15,9 @@ public class Solution {
         ListNode p2=headB;
  
         while(p1!=p2){
-            if(p1==null){
-                p1=headB;
-            }else{
-                p1=p1.next;
-            }
-            if(p2==null){
-                p2=headA;
-            }else{
-                p2=p2.next;
-            }
+            p1 = p1==null ? headA: p1.next;
+            p2 = p2==null ? headB: p2.next;
+        
         }
         return p2;
     }
