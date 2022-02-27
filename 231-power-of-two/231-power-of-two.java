5a5e6fef09e6e5664a7063d95ca6d9f569211a71
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-02-27 22:35:02 +0600

    Time: 2 ms (63.06%), Space: 41.3 MB (26.55%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n==0){
+        if(n==0) return false;
+        if(n==1){
+            return true;
+        }
+        if(n%2!=0){
             return false;
         }
-        while(n!=1){
-            if(n%2!=0){
-                return false;
-            }
-            n=n/2;
-        }
-        return true;
+        n=n/2;
+        return isPowerOfTwo(n);
     }
 }
