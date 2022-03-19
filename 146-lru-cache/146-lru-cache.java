5d5fd6345bcdbbe28f8da5584e26005b766224df
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-03-20 05:51:52 +0600

    Time: 76 ms (64.57%), Space: 120.9 MB (80.68%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,59 +1,93 @@
 class LRUCache {
-  class ListNode{
-    ListNode prev, next;
-    int key, value;
-    ListNode(int key, int value) {
-      this.key = key;
-      this.value = value;
-      }
-    }
-    
-  ListNode head = new ListNode(-1, -1);
-  ListNode tail = new ListNode(-1, -1);
-  Map<Integer, ListNode> map = new HashMap();
-  int capacity;
-  
+
+  private final Item[] itemList;
+  private final int capacity;
+  private int size;
+  private Item head;
+  private Item tail;
+
   public LRUCache(int capacity) {
     this.capacity = capacity;
-    head.next = tail;
-    tail.prev = head;
+    this.size = 0;
+    this.itemList = new Item[10001];
   }
 
   public int get(int key) {
-    if(map.containsKey(key)) {
-      ListNode node = map.get(key);
-      remove(node);
-      insert(node);
-      return node.value;
+    Item item = itemList[key];
+    if (item == null) return -1;
+    refresh(item);
+    return  item.value;
+  }
+
+  private void refresh(Item item) {
+    if (item==head) return;      // got head, do nothing --- tricky point
+
+    if(item != tail) {
+      item.next.prev = item.prev; // 2
+      item.prev.next = item.next; // 1
     } else {
-      return -1;
+      // item is tail, (also item is not head, so here head is not tail)
+      item.prev.next = null;     // 1
+      tail = item.prev;
     }
+
+    item.prev = null;           // 4
+    item.next = head;           // 3
+    head.prev = item;           // 5
+
+    head = item;
   }
 
   public void put(int key, int value) {
-    if(map.containsKey(key)) {
-      remove(map.get(key));
+    Item existItem = itemList[key];
+    // do not add new item if already exist ---------------- tricky point
+    if(existItem != null) {     
+      existItem.value = value;
+      refresh(existItem);
+      return;
     }
-    if(map.size() == capacity) {
-      remove(tail.prev);
+
+
+    Item item = Item.create(key, value);
+    itemList[key] = item;
+
+    // set head
+    Item oldHead = head;
+    head = item;
+    item.next = oldHead;
+    if (oldHead == null) {
+      // putting first element, we set tail as well
+      tail = item;
     }
-    //ListNode node = new ListNode(key, value);
-    //insert(node);
-    insert(new ListNode(key, value));
+    else {
+      oldHead.prev = item;
+    }
+
+    if (size==capacity) { // evict least recent used item
+      itemList[tail.key] = null;
+      tail = tail.prev;
+      tail.next = null;
+    } else {
+      size = size + 1;
+    }
+
   }
-  
-  private void remove(ListNode node) {
-    map.remove(node.key);
-    node.prev.next = node.next;
-    node.next.prev = node.prev;
+}
+
+class Item {
+
+  public Integer key;
+  public Integer value;
+  public Item prev;
+  public Item next;
+
+  private Item(final Integer key, final Integer value) {
+    this.key = key;
+    this.value = value;
   }
-  
-  private void insert(ListNode node){
-    map.put(node.key, node);
-    node.next = head.next;
-    node.next.prev = node;
-    head.next = node;
-    node.prev = head;
+
+  public static Item create(int key, int value) {
+    return new Item(key, value);
   }
 }
 
