f96a76b2dc8f16a8b8a6c3ce98aeba63b6bfb4da
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-03-20 05:32:12 +0600

    Time: 144 ms (8.51%), Space: 134.9 MB (27.54%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,17 +1,27 @@
-class LRUCache {  
-  Node head = new Node(0, 0), tail = new Node(0, 0);
-  Map<Integer, Node> map = new HashMap();
+class LRUCache {
+  class ListNode{
+    ListNode prev, next;
+    int key, value;
+    ListNode(int key, int value) {
+      this.key = key;
+      this.value = value;
+      }
+    }
+    
+  ListNode head = new ListNode(-1, -1);
+  ListNode tail = new ListNode(-1, -1);
+  Map<Integer, ListNode> map = new HashMap();
   int capacity;
   
-  public LRUCache(int _capacity) {
-    capacity = _capacity;
+  public LRUCache(int capacity) {
+    this.capacity = capacity;
     head.next = tail;
     tail.prev = head;
   }
 
   public int get(int key) {
     if(map.containsKey(key)) {
-      Node node = map.get(key);
+      ListNode node = map.get(key);
       remove(node);
       insert(node);
       return node.value;
@@ -27,30 +37,23 @@ class LRUCache {
     if(map.size() == capacity) {
       remove(tail.prev);
     }
-    insert(new Node(key, value));
+    //ListNode node = new ListNode(key, value);
+    //insert(node);
+    insert(new ListNode(key, value));
   }
   
-  private void remove(Node node) {
+  private void remove(ListNode node) {
     map.remove(node.key);
     node.prev.next = node.next;
     node.next.prev = node.prev;
   }
   
-  private void insert(Node node){
+  private void insert(ListNode node){
     map.put(node.key, node);
     node.next = head.next;
     node.next.prev = node;
     head.next = node;
     node.prev = head;
-  }
-  
-  class Node{
-    Node prev, next;
-    int key, value;
-    Node(int _key, int _value) {
-      key = _key;
-      value = _value;
-    }
   }
 }
 
