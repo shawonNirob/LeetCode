7440d03bcc180f6396c5ba3e07bb185697e8a824
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-04-02 06:49:33 +0600

    Time: 13 ms (5.47%), Space: 57.1 MB (34.01%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,14 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left=0;
-        int right = s.length-1;
-        recursive(s, left, right);
-    }
-    public void recursive(char[] s, int left, int right){
-        if(left>=right) return;
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        recursive(s, left+1, right-1);
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length; i++){
+            stack.push(s[i]);
+        }
+        for(int i=0; i<s.length; i++){
+            s[i] = stack.pop();
+        }
     }
 }
