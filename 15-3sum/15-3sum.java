9763077810f6aec89b17da155d20e0ea05b792cf
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-04-08 12:36:24 +0600

    Time: 29 ms (65.81%), Space: 58.6 MB (67.12%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,28 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length && nums[i] <= 0; ++i){
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                twoSumII(nums, i, res);
+        for(int i=0; i<nums.length && nums[i] <= 0; i++){
+            if(i==0 || nums[i] != nums[i-1]){
+                twoSumII(nums, i, list);
             }
         }
-        return res;
+        return list;
     }
-    void twoSumII(int[] nums, int i, List<List<Integer>> res) {
-        int lo = i + 1, hi = nums.length - 1;
-        while (lo < hi) {
-            int sum = nums[i] + nums[lo] + nums[hi];
-            if (sum < 0) {
-                ++lo;
-            } else if (sum > 0) {
-                --hi;
-            } else {
-                res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                lo++;
-                hi--;
-                while (lo < hi && nums[lo] == nums[lo - 1])
-                    ++lo;
+    public void twoSumII(int[] nums, int i, List<List<Integer>> list){
+        int low = i+1, high = nums.length-1;
+        while(low<high){
+            int sum = nums[i] + nums[low] + nums[high];
+            if(sum==0){
+                list.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                low++; high--;
+                while(low < high && nums[low]==nums[low-1]){
+                    low++;
+                }
+            }else if(sum>0){
+                high--;
+            }else{
+                low++;
             }
         }
     }
