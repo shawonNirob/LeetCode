c588f409b2f798e7f8fd3b81a733ca24e8760348
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-07-19 09:33:50 +0600

    Time: 0 ms (100.00%), Space: 41.9 MB (88.39%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -14,27 +14,21 @@
  * }
  */
 class Solution {
+    int sum=0;
+    
     public int sumRootToLeaf(TreeNode root) {
-        int totalSum=0, currSum=0;
-        Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
-        stack.push(new Pair(root, 0));
+        preOrder(root, 0);
+        return sum;
+    }
+    public void preOrder(TreeNode root, int currSum){
+        if(root==null ) return;
+        currSum = (currSum<<1) | root.val;
         
-        while(!stack.isEmpty()){
-            Pair<TreeNode, Integer> curr = stack.pop();
-            root = curr.getKey();
-            currSum = curr.getValue();
-            
-            if(root!=null){
-                currSum = currSum*2 + root.val;
-                if(root.left==null && root.right==null){
-                    totalSum += currSum;
-                }else{
-                    stack.push(new Pair(root.right, currSum));
-                    stack.push(new Pair(root.left, currSum));
-                }
-            }
-            
+        if(root.left==null && root.right==null){
+            sum += currSum;
         }
-        return totalSum;
+        
+        preOrder(root.left, currSum);
+        preOrder(root.right, currSum);
     }
 }
