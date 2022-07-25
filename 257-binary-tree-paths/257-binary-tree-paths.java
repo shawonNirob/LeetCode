a4bdf32bd227fa60e502b1a2ce88d7e2cc967ba9
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-07-25 09:05:54 +0600

    Time: 7 ms (68.15%), Space: 43.7 MB (13.61%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -16,21 +16,26 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> list = new ArrayList();
-        String path;
-        preorder(root, list,"");
+        Deque<Pair<TreeNode, String>> stack = new ArrayDeque();
+        stack.push(new Pair(root, ""));
+        String path = "";
         
-        return list;
-    }
-    public void preorder(TreeNode root,List<String> list, String path){
-        if(root==null) return;
-        path += String.valueOf(root.val);
-        if(root.left==null && root.right==null){
-            list.add(path);
-        }else{
-            path += "->";
+        while(!stack.isEmpty()){
+            Pair<TreeNode, String> curr = stack.pop();
+            root = curr.getKey();
+            path = curr.getValue();
+            
+            path += String.valueOf(root.val);
+            if(root.left==null && root.right==null){
+                list.add(path);
+
+            }else{
+                path += "->";
+            }
+            
+            if(root.right!=null) stack.push(new Pair(root.right, path));
+            if(root.left!=null) stack.push(new Pair(root.left, path));
         }
-        
-        if(root.left!=null) preorder(root.left, list, path);
-        if(root.right!=null) preorder(root.right, list, path);
+        return list;
     }
 }
