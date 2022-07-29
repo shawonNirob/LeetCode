4f88d6e5029b35538bf1f31e8663a0d1826cb4c2
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-07-29 19:49:34 +0600

    Time: 7 ms (5.12%), Space: 43.8 MB (55.24%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -13,15 +13,31 @@
  *     }
  * }
  */
-class Solution{    
-    public boolean isValidBST(TreeNode root) {    
-        return validate(root, null, null);
+class Solution{
+    public Stack<TreeNode> stack = new Stack();
+    public Stack<Integer> lowLimits = new Stack();
+    public Stack<Integer> highLimits = new Stack();
+    
+    public void update(TreeNode root, Integer low, Integer high){
+        stack.push(root);
+        lowLimits.push(low);
+        highLimits.push(high);
     }
-    public boolean validate(TreeNode root, Integer min, Integer max){
+    
+    public boolean isValidBST(TreeNode root) {    
         if(root==null) return true;
+        Integer low=null, high=null;
+        update(root,low,high);
         
-        if((min != null && root.val <= min) || (max != null && root.val >= max)) return false;
-        
-        return validate(root.left, min, root.val) && validate(root.right, root.val, max);
+        while(!stack.isEmpty()){
+            root = stack.pop();
+            low = lowLimits.pop();
+            high = highLimits.pop();
+            
+            if((low != null && root.val <= low) || (high != null && root.val >= high)) return false;
+            if(root.right != null) update(root.right, root.val, high);
+            if(root.left != null) update(root.left, low, root.val);
+        }
+        return true;
     }
 }
