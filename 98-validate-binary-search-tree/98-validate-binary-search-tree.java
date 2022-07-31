6b7d025e26154f8a3eb5138d63c813b20a1f8314
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-07-31 07:48:19 +0600

    Time: 3 ms (14.68%), Space: 45 MB (5.82%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,18 +15,21 @@
  */
 class Solution {
     public Integer prev = null;
-    
     public boolean isValidBST(TreeNode root) {
-        return inorder(root);
-    }
-    public boolean inorder(TreeNode root){
-        if(root==null) return true;
+       Stack<TreeNode> stack = new Stack();
+       TreeNode curr = root;
         
-        if(!inorder(root.left)) return false;
-        
-        if(prev != null && root.val <= prev) return false;
-        
-        prev = root.val;
-        return inorder(root.right);
+        while(curr != null || !stack.isEmpty()){
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            if(prev != null && curr.val <= prev) return false;
+            
+            prev = curr.val;
+            curr = curr.right;
+        }
+        return true;
     }
 }
