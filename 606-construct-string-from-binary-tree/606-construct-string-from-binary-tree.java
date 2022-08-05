e6143dc18c4140381320329ec8d55c215fdec55b
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-08-05 11:02:24 +0600

    Time: 18 ms (66.94%), Space: 47 MB (62.96%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -14,24 +14,28 @@
  * }
  */
 class Solution {
-     public String tree2str(TreeNode t) {
-        StringBuilder sb = new StringBuilder();
-        helper(sb,t);
-        return sb.toString();
-    }
-    public void helper(StringBuilder sb,TreeNode t){
-        if(t!=null){
-            sb.append(t.val);
-            if(t.left!=null||t.right!=null){
-                sb.append("(");
-                helper(sb,t.left);
-                sb.append(")");
-                if(t.right!=null){
-                    sb.append("(");
-                helper(sb,t.right);
-                sb.append(")");
+    public String tree2str(TreeNode root) {
+        StringBuilder str = new StringBuilder();
+        Stack<TreeNode> stack = new Stack();
+        Set<TreeNode> isVisited = new HashSet();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            root = stack.peek();
+                if(isVisited.contains(root)){
+                    stack.pop();
+                    str.append(")");
+                }else{
+                    isVisited.add(root);
+                    str.append("(" + root.val);
+                    if(root.left == null && root.right != null){
+                       str.append("()");
+                    }
+                    if(root.right != null) stack.push(root.right);
+                    if(root.left != null) stack.push(root.left);
                 }
-            }
         }
+        
+        return str.substring(1, str.length() -1);
     }
 }
