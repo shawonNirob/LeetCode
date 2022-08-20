94fc3260b4239a1fd13e35516c6e1dd1e38569b9
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-08-21 02:41:33 +0600

    Time: 1 ms (80.10%), Space: 43.2 MB (44.73%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,41 +10,20 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null && list2==null) return null;
-        if(list1==null) return list2;
-        if(list2==null) return list1;
+        ListNode head = new ListNode(-101);
+        ListNode curr = head;
         
-        ListNode damiNode =new ListNode(-101);
-        ListNode head = damiNode;
-        ListNode curr;
-           
-        while(list1 !=null && list2 != null){
-                if(list1.val == list2.val){
-                    curr = new ListNode(list1.val);
-                    damiNode.next = curr;
-                    damiNode = damiNode.next;
-                    list1 = list1.next;
-                    
-                    curr = new ListNode(list2.val);
-                    damiNode.next = curr;
-                    damiNode = damiNode.next;
-                    list2 = list2.next;
-                }
-                else if(list1.val < list2.val){
-                    curr = new ListNode(list1.val);
-                    damiNode.next = curr;
-                    damiNode = damiNode.next;
-                    list1 = list1.next;
-                }else{
-                    curr = new ListNode(list2.val);
-                    damiNode.next = curr;
-                    damiNode = damiNode.next;
-                    list2 = list2.next;
-                }   
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                curr.next = list1;
+                list1 = list1.next;
+            }else{
+                curr.next = list2;
+                list2 = list2.next;
             }
-        
-        if(list1 != null) damiNode.next = list1;
-        if(list2 != null) damiNode.next = list2;
+            curr = curr.next;
+        }
+        curr.next = (list1 == null) ? list2 : list1;
         
         return head.next;
     }
