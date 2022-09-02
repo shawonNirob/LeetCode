873b7747b8e58bd2229b6e3122c33119973f6d4a
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-03 01:00:29 +0600

    Time: 2 ms (62.27%), Space: 44 MB (49.31%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -14,27 +14,18 @@
  * }
  */
 class Solution {
-    private int preIndex;
-    private HashMap<Integer, Integer> map;
-    public TreeNode constructFromPrePost(int[] pre, int[] post) {
-        map = new HashMap<>();
-        preIndex = 0;
-        for(int i = 0; i < post.length; i++)
-            map.put(post[i], i);
-        return helper(pre, 0, pre.length-1);
-    }
-    
-    private TreeNode helper(int[] pre, int left, int right) {
-        if(left > right) return null;
-    
-        TreeNode root = new TreeNode(pre[preIndex++]);
-        if(left == right) return root;
-        
-        int mid = map.get(pre[preIndex]);
-        
-        root.left = helper(pre, left, mid);
-        root.right = helper(pre, mid+1, right-1);
-        
-        return root;
+      public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(new TreeNode(pre[0]));
+        for (int i = 1, j = 0; i < pre.length; ++i){
+            TreeNode root = new TreeNode(pre[i]);
+            while (deque.getLast().val == post[j]) {
+                deque.pollLast(); j++;
+            }
+            if (deque.getLast().left == null) deque.getLast().left = root;
+            else deque.getLast().right = root;
+            deque.offer(root);
+        }
+        return deque.getFirst();
     }
 }
