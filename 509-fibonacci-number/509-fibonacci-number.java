26d92a3737a4d02b77373acf958fcd43eb6e7389
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-04 04:08:12 +0600

    Time: 1 ms (42.23%), Space: 41.1 MB (25.79%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,18 @@
 class Solution {
+    Map<Integer, Integer> map;
     public int fib(int n) {
+        map = new HashMap();
+        
+        return dpFib(n);
+        
+    }
+    private int dpFib(int n){
         if(n==0) return 0;
         if(n==1) return 1;
+        if(map.containsKey(n)) return map.get(n);
         
-        return fib(n-1) + fib(n-2);
+        map.put(n, dpFib(n-1) + dpFib(n-2));
+        
+        return map.get(n);
     }
 }
