18c8c0915c461e8f6fd6d7a9fa527328d89dfaa2
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-06 00:45:40 +0600

    Time: 4 ms (86.30%), Space: 51.3 MB (59.44%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -14,14 +14,23 @@
  * }
  */
 class Solution {
-    public Set<Integer> set = new HashSet();
     public boolean findTarget(TreeNode root, int k) {
-        if(root==null) return false;
-        if(set.contains(k - root.val)){
-            return true;
-        }else{
-            set.add(root.val);
+        List<Integer> list = new ArrayList();
+        inorder(root, list);
+        int left =0, right = list.size()-1;
+        while(left < right){
+            int sum = list.get(left) + list.get(right);
+            if(sum == k)
+                return true;
+            else if(sum > k) right--;
+            else left++;
         }
-        return findTarget(root.left, k) || findTarget(root.right, k);
+        return false;
+    }
+    public void inorder(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
     }
 }
