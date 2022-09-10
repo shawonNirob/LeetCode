60c64e69009e98512432a0ec94dd23f1ac69eaa5
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-11 03:59:16 +0600

    Time: 1 ms (78.90%), Space: 50.3 MB (83.75%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums){
-        int sum=0, sumOnly=0;
+        //For example, given an input int[] nums = [3, 0, 1] which has a length of 3. The value of an int[] with length should be : 0~2. However, because of the missing element 2, there is an extra element 3 which equals nums.length takes place 2's position. Only when xor all element in nums and its index including nums.length, you can finally come up with the missing element: 3^0^1^0^1^2^3 = 2
+        
+        int ans = nums.length;
         for(int i=0; i<nums.length; i++){
-            sumOnly += nums[i];
-            sum += i;
+            ans = ans ^ i ^ nums[i];
         }
-        sum += nums.length;
-        return sum - sumOnly;
+        return ans;
     }
 }
