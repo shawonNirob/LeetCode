a64a2c52de36f48239cccda56c9d8134d094f567
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-10 22:46:43 +0600

    Time: 2 ms (42.37%), Space: 67.4 MB (30.54%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.java
+++ 938-range-sum-of-bst/938-range-sum-of-bst.java
@@ -23,13 +23,11 @@ class Solution {
     }
     public void helper(TreeNode root, int low, int high){
         if(root==null) return;
-        //if(root.val < low) return;
-        //if(root.val > high) return;
         
+        helper(root.left, low, high);
         if(root.val <= high && root.val >= low){
             sum += root.val;
         }
-        helper(root.left, low, high);
         helper(root.right, low, high);
         
     }
