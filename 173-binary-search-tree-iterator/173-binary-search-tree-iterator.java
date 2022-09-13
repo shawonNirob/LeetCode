78a6e5994d41dc03296bbdb346853cf98dbf1e38
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-13 17:18:50 +0600

    Time: 33 ms (24.53%), Space: 52.3 MB (18.31%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -14,36 +14,35 @@
  * }
  */
 class BSTIterator {
-    private List<Integer> list;
+    private Stack<TreeNode> stack;;
     private TreeNode root;
-    private int index;
     
     public BSTIterator(TreeNode root) {
         this.root = root;
-        this.index = 0;
-        this.list = new ArrayList();
-        this.helper(root);
-        
+        this.stack = new Stack();
+        this.leftMost(root);
     }
-    
-    public void helper(TreeNode root){
-        if(root==null) return;
-        helper(root.left);
-        list.add(root.val);
-        helper(root.right);
+    public void leftMost(TreeNode root){
+        while(root != null){
+            stack.push(root);
+            root = root.left;
+        }
     }
-    
     
     public int next(){
-        return list.get(index++);
+        TreeNode node = stack.pop();
+        if(node.right != null){
+            leftMost(node.right);
+        }
+        return node.val;
     }
     
     
     public boolean hasNext() {
-        if(index == list.size()){
-            return false;
-        }else{
+        if(!stack.isEmpty()){
             return true;
+        }else{
+            return false;
         }
     }
 }
