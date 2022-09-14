0a95eabd25766467156a433545d704c5db4b7f67
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-14 14:06:19 +0600

    Time: 7 ms (56.18%), Space: 42.2 MB (91.87%) - LeetHub

--- 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.java
+++ 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
-        String[] arr = preorder.split(",");
         int slot=1;
-        for(String index : arr){
-            if(--slot<0) return false;
-            if(!index.equals("#")){
+        int n = preorder.length();
+        for(int i=0; i<n; i++){
+            if(preorder.charAt(i) == ',') i++;
+            if(--slot < 0) return false;
+            if(preorder.charAt(i) != '#'){
                 slot += 2;
             }
+            while(i<n && preorder.charAt(i) != ',') i++;
         }
         return slot == 0;
     }
