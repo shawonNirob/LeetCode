c1373b17aaa601f2ff0f1d55f2634f9d7c02d33a
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-18 05:15:52 +0600

    Time: 1 ms (98.98%), Space: 43.8 MB (68.74%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -15,28 +15,24 @@
  */
 class Solution {
     private int sum;
-    private int n;
     public int averageOfSubtree(TreeNode root) {
-        if(root==null) return 0;
-        
-        int left = averageOfSubtree(root.left);
-        int right = averageOfSubtree(root.right);
-        
-        sum=0; n=0;
+        sum=0;
         helper(root);
-        if(n != 0){
-            if(sum/n == root.val){
-              left++;
-            }
-        }
-        return left+right;
+        return sum;
     }
-    public void helper(TreeNode root){
-        if(root==null) return;
+    public int[] helper(TreeNode root){
+        if(root==null) return new int[] {0,0};
+
+        int left[] = helper(root.left);
+        int right[] = helper(root.right);
         
-        n++;
-        sum += root.val;
-        helper(root.left);
-        helper(root.right);
+        int currSum = left[0]+right[0]+root.val;
+        int currCount = left[1]+right[1]+1;
+        
+        if(currSum/currCount == root.val){
+            sum++;
+        }
+            
+        return new int[] {currSum, currCount};
     }
 }
