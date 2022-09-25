00b714a30681b32e6d6aea8c5d9d66218af716d5
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-09-26 05:28:55 +0600

    Time: 89 ms (18.91%), Space: 40.5 MB (77.60%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.java
@@ -1,14 +1,20 @@
 class Solution {
-    public boolean judgeSquareSum(int c) {
+    public boolean judgeSquareSum(int c){
         double val = Math.sqrt(c);
         int sqrtC = (int)val;
 
-        
         for(int a=0; a<=sqrtC; a++){
-            double p = c - a*a;
-            double value = Math.sqrt(p);
-            int b = (int)value;
-            if(a*a + b*b == c) return true;
+            int b = c - a*a;
+            if(binarySearch(b, a, sqrtC)) return true;
+        }
+        return false;
+    }
+    public boolean binarySearch(int b, int left, int right){
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(mid * mid == b) return true;
+            else if(mid * mid > b) right = mid -1;
+            else left = mid + 1;
         }
         return false;
     }
