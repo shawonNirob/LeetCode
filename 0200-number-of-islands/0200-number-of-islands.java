23c7834ca6113939d9b0febbb099e2250f0106a7
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-01-05 19:19:13 +0600

    Time: 25 ms (9.97%), Space: 56.7 MB (51.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,36 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] r = new int[]{1,-1,0,0};
+        int[] c = new int[]{0,0,-1,1};
         int island = 0;
-        boolean[][] isVisited = new boolean[grid.length][grid[0].length];
+        boolean[][] isVisited = new boolean[m][n];
         
-        for(int i= 0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(!isVisited[i][j] && grid[i][j]=='1'){
+         for(int i=0; i<m; i++){
+             for(int j=0; j<n; j++){
+                 if(!isVisited[i][j] && grid[i][j]=='1'){
+                     Queue<int[]> queue = new LinkedList();
+                     queue.add(new int[]{i, j});
+                     isVisited[i][j] = true;
+                     
                      island++;
-                     dfs(i, j, grid.length, grid[0].length, grid, isVisited);
-                }
-            }
-        }
+                     
+                     while(!queue.isEmpty()){
+                         int[] node = queue.poll();
+                         
+                         for(int k=0; k<4; k++){
+                            int row = r[k]+node[0]; int col = c[k]+node[1];
+                             
+                            if(row >= 0 && row<m && col>=0 && col< n && !isVisited[row][col] && grid[row][col]=='1'){
+                                isVisited[row][col] = true;
+                                queue.add(new int[]{row, col});
+                            }
+                         }
+                     }
+                 }
+             }
+         }
         return island;
-    }
-    public void dfs(int row, int col, int m, int n, char[][] grid, boolean[][] isVisited){
-        if(row<0 || row >= m || col<0 || col>=n || isVisited[row][col] || grid[row][col] != '1') return;
-        isVisited[row][col] = true;
-        
-        dfs(row+1, col, m, n, grid, isVisited);
-        dfs(row-1, col, m, n, grid, isVisited);
-        dfs(row, col-1, m, n, grid, isVisited);
-        dfs(row, col+1, m, n, grid, isVisited);
     }
 }
