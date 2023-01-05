32b00b62169af91706ad3477bc4d5ed46876523a
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-01-05 15:41:03 +0600

    Time: 5 ms (31.62%), Space: 47.7 MB (23.49%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,28 +1,25 @@
 class Solution {
-    private int area;
+
     public int maxAreaOfIsland(int[][] grid){
-        int maxArea = 0;
         boolean[][] isVisited = new boolean[grid.length][grid[0].length];
+        int maxArea = 0;
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
                 if(!isVisited[i][j] && grid[i][j]== 1){
-                    area = 0;
-                    dfs(i,j,grid,isVisited,grid.length,grid[0].length);
+                    int area = 0;
+                    area = dfs(i,j,grid,isVisited,grid.length,grid[0].length);
                     maxArea = Math.max(maxArea, area);
                 }
             }
         }
         return maxArea;
     }
-    public void dfs(int row,int col,int[][] grid,boolean[][] isVisited, int m, int n){
-        if(row<0 || row>=m || col<0 ||col>=n || isVisited[row][col] || grid[row][col]!=1) return;
+    public int dfs(int row,int col,int[][] grid,boolean[][] isVisited, int m, int n){
+        if(row<0 || row>=m || col<0 ||col>=n || isVisited[row][col] || grid[row][col]!=1) return 0;
         
         isVisited[row][col] = true;
-        area += 1;
         
-        dfs(row+1,col,grid,isVisited,m,n);
-        dfs(row-1,col,grid,isVisited,m,n);
-        dfs(row,col+1,grid,isVisited,m,n);
-        dfs(row,col-1,grid,isVisited,m,n);
+        return 1 + (dfs(row+1,col,grid,isVisited,m,n) + dfs(row-1,col,grid,isVisited,m,n) + dfs(row,col+1,grid,isVisited,m,n) + dfs(row,col-1,grid,isVisited,m,n));
+
     }
 }
