0091602c12a0b04f0075c0c8375939d36e596bfb
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-01-07 00:07:52 +0600

    Time: 92 ms (9.08%), Space: 79.9 MB (67.99%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -2,18 +2,15 @@ class Solution {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         int m = grid1.length;
         int n = grid1[0].length;
-        int[] r = new int[]{1,-1,0,0};
-        int[] c = new int[]{0,0,-1,1};
+        int[] r = new int[]{1,0,-1,0,1};
         boolean[][] isVisited = new boolean[m][n];
         
         int result = 0;
         
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(isVisited[i][j] || grid2[i][j]==0){
-                    continue;
-                }
-
+                if(!isVisited[i][j] && grid2[i][j]==1){
+                    
                  Stack<int[]> stack = new Stack();
                  stack.push(new int[]{i,j});
                  isVisited[i][j]=true;
@@ -25,7 +22,7 @@ class Solution {
                      if(grid1[node[0]][node[1]]==0) flag = false;
 
                      for(int k=0; k<4; k++){
-                            int row = r[k]+node[0]; int col = c[k]+node[1];
+                            int row = r[k]+node[0]; int col = r[k+1]+node[1];
 
 
                             if(row>=0 && row<m && col>=0 && col<n && !isVisited[row][col] && grid2[row][col]==1){
@@ -35,6 +32,7 @@ class Solution {
                         }
                     }
                  if(flag) ++result;
+                }
             }
         }
         return result;
