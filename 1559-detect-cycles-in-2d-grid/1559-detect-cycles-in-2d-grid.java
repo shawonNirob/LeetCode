a911088b5fc6947628d9bb6b9b9f5cc6fd49586f
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-01-10 23:34:29 +0600

    Time: 28 ms (68.44%), Space: 104.8 MB (63.50%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,45 +1,25 @@
 class Solution {
-    int[] DIR_X = {1, -1, 0, 0};
-    int[] DIR_Y = {0, 0, 1, -1};
-	
-    private boolean dfs(int curX, int curY, int lastX, int lastY, int n, int m, boolean[][] vis, char[][] grid, char startChar) {
-        vis[curX][curY] = true;
-        boolean hasCycle = false;
-        // Visit all directions
-        for (int i = 0; i < 4; ++i) {
-            int newX = curX + DIR_X[i];
-            int newY = curY + DIR_Y[i];
-            // Valid point?
-            if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
-                // Don't visit last visited point
-                if (!(newX == lastX && newY == lastY)) {
-                    // Only visit nodes that equal start character
-                    if (grid[newX][newY] == startChar) {
-                        if (vis[newX][newY]) {
-						    // Still visited? There is a cycle.
-                            return true;
-                        } else {
-                            hasCycle |= dfs(newX, newY, curX, curY, n, m, vis, grid, startChar);
-                        }
-                    }
-                }
-            }
-        }
-        return hasCycle;
-    }
-	
     public boolean containsCycle(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
-        boolean hasCycle = false;
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[i].length; ++j) {
-                if (!vis[i][j]) {
-                    hasCycle |= dfs(i, j, -1, -1, n, m, vis, grid, grid[i][j]);
-                }
+        int[][] visit = new int[grid.length][grid[0].length];
+        
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(visit[i][j]==0 && dfs(i,j,i,j, visit,grid, 1, grid[i][j])) return true;
             }
         }
-        return hasCycle;
+                   
+        return false;
+    }
+    public boolean dfs(int r,int c,int prevR, int prevC,  int[][] visit, char[][] grid, int count, char word){
+        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]!=word) return false;
+        
+        if(visit[r][c] - visit[prevR][prevC] >= 3) return true;
+        
+        if(visit[r][c]!=0) return false;
+        
+        visit[r][c] = count;
+        
+        return dfs(r+1, c, r, c, visit, grid, count+1, word) || dfs(r-1, c,r,c, visit, grid, count+1, word) || dfs(r, c+1,r,c, visit, grid, count+1, word) || dfs(r, c-1,r,c, visit, grid, count+1, word);
+        
     }
 }
