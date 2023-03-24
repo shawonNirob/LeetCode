a6cf4a42411f721c5952e1f775ad370353bcbc27
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-03-25 01:25:23 +0600

    Time: 3 ms (91.51%), Space: 42.8 MB (56.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,47 +1,37 @@
-public class Solution {
-        public boolean canFinish(int numCourses, int[][] prerequisites) {
-    if(numCourses == 0 || prerequisites == null || prerequisites.length == 0) return true; //??
-    
-    // create the array lists to represent the courses
-    List<List<Integer>> courses = new ArrayList<List<Integer>>(numCourses);
-    for(int i=0; i<numCourses; i++) {
-        courses.add(new ArrayList<Integer>());
-    }
-    
-    // create the dependency graph
-    for(int i=0; i<prerequisites.length; i++) {
-        courses.get(prerequisites[i][1]).add(prerequisites[i][0]);
-    }
-
-    int[] visited = new int[numCourses]; 
-    
-    // dfs visit each course
-    for(int i=0; i<numCourses; i++) {
-           if (!dfs(i,courses, visited)) return false; 
-    }
-    
-    return true;
-}
-
-private boolean dfs(int course, List<List<Integer>> courses, int[] visited) {
-    
-    visited[course] = 1; // mark it being visited
-    
-    List<Integer> eligibleCourses = courses.get(course); // get its children
-    
-    // dfs its children
-    for(int i=0; i<eligibleCourses.size(); i++) {
-        int eligibleCourse = eligibleCourses.get(i).intValue();
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        LinkedList<Integer> adj[] = new LinkedList[numCourses];
         
-        if(visited[eligibleCourse] == 1) return false; // has been visited while visiting its children - cycle !!!!
-        if(visited[eligibleCourse]  == 0) { // not visited
-           if (!dfs(eligibleCourse,courses, visited)) return false; 
+        for(int i=0; i<numCourses; i++){
+            adj[i] = new LinkedList<>();
         }
-
+        
+        for(int[] pre : prerequisites){
+            adj[pre[0]].add(pre[1]);
+        }
+        
+        int[] visited = new int[numCourses];
+        
+        for(int i=0; i<numCourses; i++){
+            if(!dfs(i, visited, adj)){
+               return false;
+            }
+        }
+        return true;
     }
-    
-    visited[course] = 2; // mark it done visiting
-    return true;
-    
+    public boolean dfs(int node, int[] visited, LinkedList<Integer> adj[]){
+        if(visited[node]==1) return false;
+        if(visited[node]==2) return true;
+        
+        visited[node]=1;
+        
+        for(int i : adj[node]){
+            if(!dfs(i, visited, adj)){
+               return false;
+            }
+        }
+        visited[node]=2;
+        
+        return true;
+    }
 }
-    }
