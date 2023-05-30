5ee5b01900abbfa321a346420ddad19e76f5bb35
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-05-30 13:18:30 +0600

    Time: 1 ms (99.00%), Space: 44.2 MB (9.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,34 +1,33 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean[] visited = new boolean[n];
-        int provinces =0;
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                provinces++;
-                bfs(i, isConnected, visited);
-            }
-        }
-        return provinces;
-    }
-    public void bfs(int node, int[][] isConnected, boolean[] visited){
+    public void bfs(int node, int[][] isConnected, boolean[] visit) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        
-        while(!q.isEmpty()){
+        q.offer(node);
+        visit[node] = true;
+
+        while (!q.isEmpty()) {
             node = q.poll();
-            visited[node] = true;
-            
-            for(int i=0; i<isConnected.length; i++){
-                if(isConnected[node][i] == 1 && !visited[i]){
-                    q.add(i);
+
+            for (int i = 0; i < isConnected.length; i++) {
+                if (isConnected[node][i] == 1 && !visit[i]) {
+                    q.offer(i);
+                    visit[i] = true;
                 }
             }
         }
     }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int numberOfComponents = 0;
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfComponents++;
+                bfs(i, isConnected, visit);
+            }
+        }
+
+        return numberOfComponents;
+    }
 }
-              
-              
-              
-              
