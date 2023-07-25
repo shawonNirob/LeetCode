99344335bd4b4e74d1f01804897ccd4cef7e3ea2
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-07-25 18:35:07 +0600

    Time: 14 ms (47.77%), Space: 49.6 MB (36.98%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countPaths(int n, int[][] roads) {
-        List<List<Pair>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+        List<List<Pair>> adj = new ArrayList();
+        for(int i=0; i<n; i++){
+            adj.add(new ArrayList());
         }
         
         int m = roads.length;
-        for(int i = 0; i < m; i++) {
+        for(int i=0; i<m; i++){
             adj.get(roads[i][0]).add(new Pair(roads[i][1], roads[i][2]));
             adj.get(roads[i][1]).add(new Pair(roads[i][0], roads[i][2]));
         }
@@ -19,43 +19,40 @@ class Solution {
         distance[0] = 0;
         
         PriorityQueue<Pair> q = new PriorityQueue<>(Comparator.comparingLong(Pair -> Pair.time));
-        q.add(new Pair(0, 0));
+        q.add(new Pair(0,0));
         
-        int mod = (int)(1e9 + 7);
+        int mod = (int) (1e9+7);
         
-        while(!q.isEmpty()) {
+        while(!q.isEmpty()){
             Pair pointer = q.poll();
             int u = pointer.dest;
             long dist = pointer.time;
             
-            if (dist > distance[u]) {
-                continue; // Skip outdated nodes
-            }
+            if(dist > distance[u]) continue;
             
-            for(Pair next : adj.get(u)) {
+            for(Pair next : adj.get(u)){
                 int v = next.dest;
                 long costUV = next.time;
                 
-                if(dist + costUV < distance[v]) {
+                if(dist + costUV < distance[v]){
                     distance[v] = dist + costUV;
                     ways[v] = ways[u];
-                    q.add(new Pair(v, dist + costUV));
+                    q.add(new Pair(v,dist + costUV));
                 }
-                else if(dist + costUV == distance[v]) {
-                    ways[v] = (ways[v] + ways[u]) % mod;
+                else if(dist + costUV == distance[v]){
+                    ways[v] = (ways[v]+ ways[u]) % mod;
                 }
             }
         }
         
-        return (int)(ways[n - 1] % mod);
+        return (int)(ways[n-1] % mod);
+        
     }
 }
-
-class Pair {
+class Pair{
     int dest;
     long time;
-    
-    public Pair(int dest, long time) {
+    public Pair(int dest, long time){
         this.dest = dest;
         this.time = time;
     }
