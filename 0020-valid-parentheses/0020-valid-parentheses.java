81a2734bbc580192fcb76dcf8955b7f50ddc63ba
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2023-08-14 23:42:07 +0600

    Time: 2 ms (89.64%), Space: 40.3 MB (86.56%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        
+        for(int i=0; i<s.length(); i++){
+            char chr = s.charAt(i);
+                
+            if(chr == '(' ) stack.push(')');
+            else if(chr == '{' ) stack.push('}');
+            else if(chr == '[' ) stack.push(']');
+            else if(stack.isEmpty() || stack.pop() != chr) return false; 
+        }
+        return stack.isEmpty();
+    }
+}
