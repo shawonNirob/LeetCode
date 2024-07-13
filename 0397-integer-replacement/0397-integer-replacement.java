03369069854a69f8445646517eec525836e9825d
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2024-07-13 17:23:24 +0600

    Time: 1 ms (67.57%), Space: 40.6 MB (21.09%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,16 +1,22 @@
 class Solution {
+    Map<Long, Integer> map = new HashMap();
     public int integerReplacement(int n) {
         return findMin(n);
     }
     public int findMin(long n){
         if(n==1) return 0;
+        if(map.containsKey(n)) return map.get(n);
         
         if(n%2==0){
-            return 1+findMin(n/2);
+            int step = 1+findMin(n/2);
+            map.put(n, step);
+            return step;
         }else{
             int opt1 = 1+findMin(n+1);
             int opt2 = 1+findMin(n-1);
-            return Math.min(opt1, opt2);
+            int step = Math.min(opt1, opt2);
+            map.put(n, step);
+            return step;
         }
     }
 }
