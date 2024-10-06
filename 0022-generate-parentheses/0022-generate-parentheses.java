dcfd2a97fd4b82e8f30a6fd19fdf5221540e7a47
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2024-10-07 01:43:48 +0600

    Time: 1 ms (86.34%), Space: 43 MB (62.79%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,38 +1,23 @@
 class Solution{
     public List<String> generateParenthesis(int n){
-        StringBuilder str = new StringBuilder();
-        str.append('(');
 
         List<String> list = new ArrayList();
-        callParenthesis(list, 1 , 0, n, str);
+        callParenthesis(list, 1 , 0, n, "(");
 
         return list;
     }
-    public void callParenthesis(List<String> list, int open, int close, int n, StringBuilder str){
-        if(open+close == n*2 && isValid(str)){
-            list.add(str.toString());
+    public void callParenthesis(List<String> list, int open, int close, int n, String str){
+        if(open+close == n*2){
+            list.add(str);
             return;
         }
 
         if(open < n){
-            str.append('(');
-            callParenthesis(list, open+1, close, n, str);
-            str.deleteCharAt(str.length()-1);
+            callParenthesis(list, open+1, close, n, str+"(");
         }
 
-        if(close < n){
-            str.append(')');
-            callParenthesis(list, open, close+1, n, str);
-            str.deleteCharAt(str.length()-1);
+        if(close < open){
+            callParenthesis(list, open, close+1, n, str+")");
         }
-    }
-    public boolean isValid(StringBuilder str){
-        int open = 0;
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i) == '(') open++;
-            if(str.charAt(i) == ')') open--;
-            if(open < 0) return false;
-        }
-        return true;
     }
 }
