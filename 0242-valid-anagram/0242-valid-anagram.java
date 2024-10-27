2f3b756d5609effb7bf5de0446e3fcbf41effdce
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2024-10-28 03:57:14 +0600

    Time: 17 ms (20.04%), Space: 44.1 MB (60.79%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap();
+
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+
+        for(int i=0; i<t.length(); i++){
+            char chr = t.charAt(i);
+
+            if(!map.containsKey(chr) || map.get(chr) == 0){
+                return false;
+            }else{
+                map.put(chr, map.get(chr) - 1);
+            }
+        }
+        
+        for(int check : map.values()){
+            if(check != 0) return false;
+        }
+
+        return true;
+    }
+}
