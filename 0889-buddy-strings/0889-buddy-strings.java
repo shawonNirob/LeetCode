635ae3a4cf26208c646a57cccd6fb80bf2728d5e
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2024-11-01 15:13:38 +0600

    Time: 4 ms (42.8%), Space: 42.1 MB (93.11%) - LeetHub

--- 0889-buddy-strings/0889-buddy-strings.java
+++ 0889-buddy-strings/0889-buddy-strings.java
@@ -1,39 +1,29 @@
-import java.util.HashSet;
-import java.util.Set;
-
-public class Solution {
+class Solution {
     public boolean buddyStrings(String s, String goal) {
-        // Step 1: Check length
-        if (s.length() != goal.length()) {
-            return false;
-        }
-        
-        // Step 2: Check if s and goal are identical and contain duplicate characters
-        if (s.equals(goal)) {
-            Set<Character> uniqueChars = new HashSet<>();
-            for (char c : s.toCharArray()) {
-                uniqueChars.add(c);
+        if(s.length() != goal.length()) return false;
+
+        //check identical and duplicate
+        if(s.equals(goal)){
+            Set<Character> set = new HashSet();
+            for(char chr : s.toCharArray()){
+                set.add(chr);
             }
-            return uniqueChars.size() < s.length(); // True if there's at least one duplicate character
+
+            return set.size() < s.length();
         }
-        
-        // Step 3: Identify mismatched positions
-        int first = -1, second = -1;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != goal.charAt(i)) {
-                if (first == -1) {
-                    first = i;
-                } else if (second == -1) {
-                    second = i;
-                } else {
-                    return false; // More than two mismatches
-                }
+
+        //check the position of two unmatch
+        int first = -1; int second = -1;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) != goal.charAt(i)){
+                if(first == -1) first = i;
+                else if(second == -1) second = i;
+                else return false; //Should not contains more than two miosmatch
             }
         }
-        
-        // Step 4: Ensure two mismatches exist and are swappable
-        return second != -1 &&
-               s.charAt(first) == goal.charAt(second) &&
-               s.charAt(second) == goal.charAt(first);
+
+        //Check that two can me swapable
+
+        return second != -1 && s.charAt(first) == goal.charAt(second) &&  s.charAt(second) == goal.charAt(first);
     }
 }
