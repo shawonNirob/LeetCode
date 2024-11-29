d262e8e7ba36e3ba8847e070fae270418be63e95
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2024-11-29 19:57:12 +0600

    Time: 10 ms (63.43%), Space: 45.5 MB (35.89%) - LeetHub

--- 1508-longest-happy-prefix/1508-longest-happy-prefix.java
+++ 1508-longest-happy-prefix/1508-longest-happy-prefix.java
@@ -1,23 +1,28 @@
 public class Solution {
     public String longestPrefix(String s) {
+        if(s.length() <= 1) return "";
+
         int n = s.length();
         int[] lps = new int[n];
-        int length = 0; 
 
-  
-        for (int i = 1; i < n; i++) {
-            while (length > 0 && s.charAt(i) != s.charAt(length)) {
-                length = lps[length - 1];
-            }
-            if (s.charAt(i) == s.charAt(length)) {
-                length++;
-                lps[i] = length;
-            } else {
-                lps[i] = 0;
+        int len = 0;
+        int i = 1;
+
+        while(i < n){
+            if(s.charAt(i) == s.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len != 0){
+                    len = lps[len - 1];
+                }else{
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
 
-
-        return s.substring(0, lps[n - 1]);
+        return s.substring(0, lps[n-1]);
     }
 }
