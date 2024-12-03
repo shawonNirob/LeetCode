9b7ceded5ea585284355498b7d3b21c5256bc914
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2024-12-04 05:54:34 +0600

    Time: 263 ms (58.28%), Space: 42.6 MB (47.57%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,20 +1,17 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int repeatCount = (b.length() + a.length() - 1) / a.length(); // Minimum repeats
+        int repeatCount = (b.length() + a.length() - 1) / a.length(); 
         StringBuilder repeatedA = new StringBuilder(a.repeat(repeatCount));
         
-        // Check if b is a substring of the current repeated string
         if (repeatedA.toString().contains(b)) {
             return repeatCount;
         }
 
-        // Check with one more repetition
         repeatedA.append(a);
         if (repeatedA.toString().contains(b)) {
             return repeatCount + 1;
         }
-
-        // If b is still not found
+        
         return -1;
     }
 }
