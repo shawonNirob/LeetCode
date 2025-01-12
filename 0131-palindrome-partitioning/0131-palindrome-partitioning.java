c9d6280dad9a8a15741041bdaa31271bfc172e9c
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2025-01-12 23:39:50 +0600

    Time: 8 ms (58.64%), Space: 56.8 MB (63.75%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,10 +1,11 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList();;
+    public List<List<String>> partition(String s){
+        List<List<String>> ans = new ArrayList();
         List<String> list = new ArrayList();
         helper(0, s, ans, list);
         return ans;
     }
+
     public void helper(int index, String s,List<List<String>> ans, List<String> list){
         if(index == s.length()){
             ans.add(new ArrayList(list));
@@ -19,6 +20,7 @@ class Solution {
             }
         }
     }
+
     public boolean isPalindrome(String s, int first, int last){
         while(first<=last){
             if(s.charAt(first) != s.charAt(last)){
