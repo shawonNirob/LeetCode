49949735ee8e78960fed39a00adb70dc0ba5716d
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2025-04-21 19:59:28 +0600

    Time: 0 ms (100%), Space: 40.4 MB (49.28%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean canWinNim(int n) {
         if(n==1) return true;
-        if(n==2) return true;
-        if(n==3) return true;
-
-        if(n % 4 == 0) return false;
-
-        return true;
+        else if(n==2) return true;
+        else if(n==3) return true;
+        else if(n%4==0) return false;
+        else return true;
     }
 }
